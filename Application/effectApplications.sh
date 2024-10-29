45576bc53d4ac1a80a74eaf20aa4c81dc9968810
mmercer@trivir.com <mmercer@trivir.com>
2024-10-29 14:21:55 -0600

    add applications to be deleted tests

--- Application/effectApplications.sh
+++ Application/effectApplications.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 frodo oauth client import -a -f AddService.application.json golden1 root
-
+frodo oauth client import -a -f DeleteMe.application.json golden1 root
+frodo oauth client delete -i 'Add_me' golden1 root
